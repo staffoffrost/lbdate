d811172a3d0472280ca78a5ada5fc968d4a35221
Leon Bernstein <unlimited.2033@gmail.com>
2020-04-14 23:40:48 +0300

    html modifiacation plus added vesion

--- www/app.component.ts
+++ www/app.component.ts
@@ -1,18 +1,23 @@
 import { lbDate, TimeZoneOptions } from 'lbdate'
 import { DateSelectionComponent } from './date-selections.component'
+import { getElementById, setValueToElement } from './dom'
 import { LbDateOptionsComponent } from './lbdate-options.component'
 import { ObservablesService } from './observables.service'
 import { SerializationResultComponent } from './serialization-result.component'
 
 export class AppComponent {
 
-  private static _app: AppComponent | null = null
-
   private constructor(
     private _observables: ObservablesService,
     private _dateSelection: DateSelectionComponent,
     private _lbDateOptions: LbDateOptionsComponent,
   ) { }
+
+  private static _app: AppComponent | null = null
+
+  private _elements = {
+    lbDateVersion: getElementById('lbDateVersion') as HTMLSpanElement,
+  }
 
   public static buildApp(): AppComponent {
     if (this._app) return this._app
@@ -33,6 +38,7 @@ export class AppComponent {
     this._lbDateOptions.init()
     this._setObservers()
     this._scopedRun()
+    this._setVersion()
   }
 
   private _setObservers(): void {
@@ -49,10 +55,19 @@ export class AppComponent {
     lbDate({
       timezone: TimeZoneOptions.manual,
       manualTimeZoneOffset: -840,
-      precision: 2
     }).run(() => {
       const result = JSON.stringify({ date: new Date() })
       this._observables.nextScopedRunResult(result)
     })
+  }
+
+  private async _setVersion(): Promise<void> {
+    try {
+      const version: string = (await fetch('version.json').then(r => r.json())).version
+      if (!version) throw new Error()
+      setValueToElement(this._elements.lbDateVersion, version)
+    } catch {
+      this._elements.lbDateVersion.parentElement?.remove()
+    }
   }
 }
