84b00e3eba1d6066f6e9951b8ff18135d503467e
Leon Bernstein <unlimited.2033@gmail.com>
2020-04-15 02:30:21 +0300

    webpack build prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const merge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = {
+const COMMON_CONFIG = {
   entry: './playground/ts/main.ts',
   devtool: 'inline-source-map',
   module: {
@@ -10,7 +12,7 @@ module.exports = {
         use: [{
           loader: 'ts-loader',
           options: {
-            configFile: "playground/ts/tsconfig.json"
+            configFile: 'playground/ts/tsconfig.json',
           }
         }],
         exclude: /node_modules/,
@@ -20,7 +22,7 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
     alias: {
-      lbdate: path.resolve(__dirname, 'src')
+      lbdate: path.resolve(__dirname, 'src'),
     }
   },
   output: {
@@ -28,3 +30,24 @@ module.exports = {
     path: path.resolve(__dirname, 'playground/www'),
   },
 }
+
+const DEV_CONFIG = {
+  devtool: 'inline-source-map',
+}
+
+const PROD_CONFIG = {
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        sourceMap: false,
+        terserOptions: {
+          mangle: true
+        },
+      })
+    ],
+  },
+}
+
+module.exports = (env, argv) => merge(COMMON_CONFIG, argv.mode === 'production' ? PROD_CONFIG : DEV_CONFIG)
