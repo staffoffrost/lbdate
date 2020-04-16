7e183c40b3f401c2887925b3dc6dbc879df34300
Leon Bernstein <unlimited.2033@gmail.com>
2020-04-16 19:23:37 +0300

    better packege json scripts

--- npm-build-success.js
+++
@@ -1,3 +0,0 @@
-console.log()
-console.log("\x1b[32m", 'LbDate build was successful.', "\x1b[0m")
-console.log()
--- npm-lint-success.js
+++
@@ -1,3 +0,0 @@
-console.log()
-console.log("\x1b[32m", 'LbDate linting was passed successfully.', "\x1b[0m")
-console.log()
