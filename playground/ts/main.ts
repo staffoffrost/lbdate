b0431d13fbbc4b2306c81e1c8559be990791ec71
Leon Bernstein <unlimited.2033@gmail.com>
2020-05-22 12:47:27 +0300

    added api key

--- playground/ts/main.ts
+++ playground/ts/main.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './components/app.component'
 import { onDomLoaded } from './utils/dom'
 
 const firebaseConfig = {
+  apiKey: 'AIzaSyB5DEtm9bf2REvtW_znFhPugO8Dtz-q19s',
   projectId: 'lbdate-dev',
   appId: '1:136590163892:web:2c7608ca199fc107a73ce1',
   measurementId: 'G-B22TXSKFST'
