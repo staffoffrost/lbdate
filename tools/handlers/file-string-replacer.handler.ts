00144796454424fe8678269c38e68db888c234cf
Leon Bernstein <unlimited.2033@gmail.com>
2020-05-30 22:00:13 +0300

    tools, better string replacer

--- tools/handlers/file-string-replacer.handler.ts
+++ tools/handlers/file-string-replacer.handler.ts
@@ -3,14 +3,13 @@ import { FileStringReplacementConfig, StringReplacementSet } from '../models'
 import { Provider } from '../provider'
 
 export function replaceStringsInFile(config: FileStringReplacementConfig): void {
-  const hash = Provider.getHashHandler().hash
   config.sets.forEach(dataSet => {
     const filePath = resolvePath(config.rootFolder, dataSet.filePath)
     let fileStr = readStrFromFile(filePath)
     if (!fileStr) throw new Error(`This file is empty: ${filePath}`)
+    dataSet.replacementSets = prepareReplacementSets(dataSet.replacementSets)
     fileStr = replaceStrings(
       dataSet.replacementSets,
-      hash,
       fileStr,
       filePath,
     )
@@ -18,9 +17,24 @@ export function replaceStringsInFile(config: FileStringReplacementConfig): void 
   })
 }
 
+function prepareReplacementSets(replacementSets: StringReplacementSet[]): StringReplacementSet[] {
+  const hash = Provider.getHashHandler().hash
+  const appDetails = Provider.getAppDetailsHandler()
+  const curAppVer = appDetails.curAppVer
+  const nextAppVer = appDetails.nextAppVer
+  const replacePlaceHolders = (str: string): string => {
+    return str.replace(/\[hash\]/g, hash)
+      .replace(/\[curAppVer\]/g, curAppVer)
+      .replace(/\[nextAppVer\]/g, nextAppVer)
+  }
+  return replacementSets.map(set => ({
+    currStr: replacePlaceHolders(set.currStr),
+    nextStr: replacePlaceHolders(set.nextStr),
+  }))
+}
+
 function replaceStrings(
   replacementSets: StringReplacementSet[],
-  hash: string,
   fileStr: string,
   filePath: string,
 ): string {
@@ -28,10 +42,9 @@ function replaceStrings(
     if (!fileStr.includes(set.currStr)) {
       throw new Error(`Cannot replace ${set.currStr} because it wasn't found in this file: ${filePath}.`)
     }
-    if (set.nextStr.includes('[hash]')) {
-      set.nextStr = set.nextStr.replace('[hash]', hash)
-    }
-    fileStr = fileStr.replace(set.currStr, set.nextStr)
+    do {
+      fileStr = fileStr.replace(set.currStr, set.nextStr)
+    } while (fileStr.includes(set.currStr))
   })
   return fileStr
 }
