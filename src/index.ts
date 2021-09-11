4706e0427916275a9b3ecc1c055ab9666659c903
Leon Bernstein <unlimited.2033@gmail.com>
2021-09-11 21:37:14 +0300

    refactor(moved all src to internal folder)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { lbDate } from './core/lbdate'
-export * from './config/time-zone-options.enum'
-export * from './interfaces/lbdate-actions.interface'
-export * from './interfaces/lbdate-options.interface'
-export * from './interfaces/lbdate.interface'
+import { lbDate } from './internal/core/lbdate'
+export * from './internal/config/time-zone-options.enum'
+export * from './internal/interfaces/lbdate-actions.interface'
+export * from './internal/interfaces/lbdate-options.interface'
+export * from './internal/interfaces/lbdate.interface'
+export * from './internal/interfaces/moment-like.interface'
 export { lbDate }
 
 export default lbDate
